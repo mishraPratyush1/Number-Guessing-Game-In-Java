ee5a528ca361b67b30e9b046af9e520a06f68476
pratyush mishra <pratyushmishra2222@gmail.com>
2020-10-24 19:19:51 +0530

    Update guess.java

--- guess.java
+++ guess.java
@@ -1,23 +1,64 @@
 import java.util.*;
-class Main{
-	
-	public static void main(String []args) throws InterruptedException{
-	
-	Scanner sc = new Scanner(System.in);
-	Random rand = new Random();
-	int num = rand.nextInt(100);
-	int chances=rand.nextInt(11);
-	System.out.println("Setting up the game for you!!!.... Please Wait!!!!!");
-	Thread.sleep(3000);
-	System.out.println("Game is Ready to be Played\n");
-	guess g;
-	if(chances!=0){
-		g = new guess(num,chances);
+class guess{
+		int n,chances;
+		int score=0;
+		int total;
+		public guess(int n,int chances){
+			this.n=n;
+			total=chances;
+			this.chances=chances-1;
+			
+		}
+		public void guessed() throws InterruptedException
+		{
+		
+		Scanner sc = new Scanner(System.in);
+		boolean guessed = false;
+		Random rand = new Random();
+		System.out.println("enter your guess ");
+		int guess=sc.nextInt();
+		while(this.total > 0 && this.chances>0)
+		{
+			System.out.println("You have "+chances+" chances left");
+			if(guess > this.n){
+				System.out.println("guess a bit lower");
+				guess = sc.nextInt();
+			}
+			else if(guess < this.n){
+				System.out.println("guess a bit higher");
+				guess = sc.nextInt();
+			}
+			else if(guess == this.n){
+				score+=((total)*100);
+				System.out.println("\nYOOhoo the number was "+n+" you have guessed right!! at the "+(total-chances)+" guess");
+				System.out.println("\nCalculating Score!!! Please Wait ");
+				Thread.sleep(3000);
+				System.out.println("\nYour Score is ");
+				Thread.sleep(2000);
+				System.out.print(this.score);
+				guessed=true;
+				break;
+			}
+			--chances;
+			--total;
+		} 
+		if(!guessed){
+			System.out.println("Analysing status!! please wait ");
+			Thread.sleep(1200);
+			System.out.println("Sorry mate!!! Better luck Next Time \n");
+			for(int i=0;i<5;i++){
+				System.out.print("\u2639"+" ");
+				Thread.sleep(500);
+			}
+			System.out.println("\n\nRevealing the Number for you!!.... \n");
+			Thread.sleep(2000);
+			System.out.println("Almost There.... \n");
+			Thread.sleep(1700);
+			System.out.println("The Number was "+this.n);
+			Thread.sleep(700);
+			System.out.println("Score 0");
+
+		}
 	}
-	else{
-		g = new guess(num,rand.nextInt(11));
-	}
-	g.guessed();
-	
-	}	
+
 }
